e2681c601ae4f3a7c27a409f97743241380b9bf6
TanguturiArchana <150064169+TanguturiArchana@users.noreply.github.com>
2026-09-01 22:25:32 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,15 @@
+
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "vmc_hmi",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10
 });
 
 module.exports = pool;
+
